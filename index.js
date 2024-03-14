b5bfaf4d2e012a6bfaefbd0c01f0131e5b76e4f1
Chrispine Oburu <chrizoburu65@gmail.com>
2024-03-14 21:57:15 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-function addingEventListener() {
+function addingEventListener(){
+    const input = document.getElementById('button');
+    input.addEventListener('click', function () {
+        console.log('I was clicked!');
+        });
+
 }
+
+addingEventListener();
